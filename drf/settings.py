6b542623cf5e6f73a353a9e8fa9bfe475cce17cb
KINANGA DENNIS <denniskinanga6@gmail.com>
2023-09-28 00:12:05 +0300

    add

--- drf/settings.py
+++ drf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(ma!0=%dn(gqh@a0q*1x5-09sp&0e^+9pwidt%+0#7aat)jrjj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'adamsite-c8e88a6bb1a1.herokuapp.com']
 
 
 # Application definition
